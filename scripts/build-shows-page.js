45402fd6f525a5579f7d724a726272aa2a3ef69f
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-05-19 22:57:33 -0700

    Uplaoding sprint-1 of project

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,127 +1,81 @@
 
-// //Content stored in Array
-// const shows =[
-// {date:"Mon Sept 06 2021",venue:"Ronald Lane",location:"San Francisco, CA" } ,
-// {date:"Tue Sept 21 2021",venue:"Pier 3 East",location:"San Francisco, CA"},
-// {date:"Fri Oct 15 2021",venue:"View Lounge",location:"San Francisco, CA"},
-// {date:"Sat Nov 06 2021",venue:"Hyatt Agency",location:"San Francisco, CA"},
-// {date:"Fri Nov 26 2021",venue:"Moscow Center",location:"San Francisco, CA"},
-// {date:"Wed Dec 15 2021",venue:"Press Club",location:"San Francisco, CA"},
-// ];
-apiKey = "5218b745-34db-4023-b4da-1a32e81e281c";
 
-function getShowDates() {
-  const showsTimes = axios.get("https://project-1-api.herokuapp.com/showdates?api_key=5218b745-34db-4023-b4da-1a32e81e281c");
+const shows =[
+{date:"Mon Sept 06 2021",venue:"Ronald Lane",location:"San Francisco, CA" } ,
+{date:"Tue Sept 21 2021",venue:"Pier 3 East",location:"San Francisco, CA"},
+{date:"Fri Oct 15 2021",venue:"View Lounge",location:"San Francisco, CA"},
+{date:"Sat Nov 06 2021",venue:"Hyatt Agency",location:"San Francisco, CA"},
+{date:"Fri Nov 26 2021",venue:"Moscow Center",location:"San Francisco, CA"},
+{date:"Wed Dec 15 2021",venue:"Press Club",location:"San Francisco, CA"},
+];
 
-  showsTimes.then(response => {
-    console.log(response.data);
-
-    response.data.sort((a, b) => {
-      return b.posted - a.posted;
-    });
-
-    displayDates(response.data);
-  });
-
-  showsTimes.catch(error => {
-    console.log(error);
-  });
-}
-getShowDates();
- 
-//Selecting classes in comment section
 const concertHead = document.querySelector('.concert-head');
-const concertList = document.querySelector(".concert-list");
-
-
-//Creating heading and appending it.
-const concertHeading = document.createElement('h2');
-concertHeading.classList.add('concert-heading');
-concertHeading.innerText= "Shows";
-concertHead.appendChild(concertHeading);
-
+const concertList = document.querySelector("#concert-list");
 
 const concert =document.createElement("div");
 concert.classList.add("concert-show");
+
+const concertHeading = document.createElement('h2');
+concertHeading.classList.add('concert-heading');
+concertHeading.innerText= "Shows";
+ // console.log(showHeading);
+//  console.log("showhead: ", showHead);
+concertHead.appendChild(concertHeading);
+
+shows.forEach(show =>{
+
+const dateHead = document.createElement("p");
+dateHead.classList.add("show-guide");
+dateHead.textContent = "DATE";
+concert.appendChild(dateHead);
+
+const date = document.createElement("p");
+date.classList.add("show-date");
+date.textContent = show.date;
+concert.appendChild(date);
+
+const venueHead = document.createElement("p");
+venueHead.classList.add("show-guide");
+venueHead.textContent = "VENUE";
+concert.appendChild(venueHead);
+
+const venue = document.createElement("p");
+venue.classList.add("show-venue");
+venue.textContent = show.venue;
+concert.appendChild(venue);
+
+const loacationHead = document.createElement("p");
+loacationHead.classList.add("show-guide");
+loacationHead.textContent = "LOCATION";
+concert.appendChild(loacationHead);
+
+const location = document.createElement("p");
+location.classList.add("show-guide");
+location.textContent = show.location;
+concert.appendChild(location);
+
+const buyButton = document.createElement("button");
+buyButton.classList.add("show-buyBtn");
+buyButton.textContent = "BUY TICKET";
+concert.appendChild(buyButton);
+
+// console.log(concert);
+// console.log(concertList);
+
 concertList.appendChild(concert);
 
-const dateDisplay = document.createElement("span");
-dateDisplay.classList.add("concert-show__display");
-dateDisplay.textContent = "DATE";
-concert.appendChild(dateDisplay);
+});
+// const words = ["Hey", "What's up", "bye"]
 
-const venueDisplay =document.createElement("span");
-venueDisplay.classList.add("concert-show__display");
-venueDisplay.textContent = "VENUE";
-concert.appendChild(venueDisplay);
+// function displayWord (word) {
+//   console.log(word)
+//    code that manipulates the DOM
+// }
 
-const locationDisplay = document.createElement("span");
-locationDisplay.classList.add("concert-show__display");
-locationDisplay.textContent = "LOCATION";
-concert.appendChild(locationDisplay);
+// function displayWords () {
+//   words.forEach(word => {
+//     displayWord(word)
+//   });
+// }
 
-const emptyDisplay = document.createElement("span");
-emptyDisplay.classList.add("concert-show__empty");
-emptyDisplay.textContent = " ";
-concert.appendChild(emptyDisplay);
-
-
-
- 
-
-//Creating elements and looping through it.
-
-function displayDates(shows){
-  shows.forEach(show =>{
-
-    const concertElement= document.createElement("div");
-    concertElement.classList.add("concert-list__element");
-    
-    const dateHead = document.createElement("p");
-    dateHead.classList.add("concert-list__guide");
-    dateHead.textContent = "DATE";
-    concertElement.appendChild(dateHead);
-    
-    const date = document.createElement("p");
-    date.classList.add("concert-list__date");
-    date.textContent = (new Date()).toLocaleDateString();
-    concertElement.appendChild(date);
-    
-    
-    const venueHead = document.createElement("p");
-    venueHead.classList.add("concert-list__guide");
-    venueHead.textContent = "VENUE";
-    concertElement.appendChild(venueHead);
-    
-    const venue = document.createElement("p");
-    venue.classList.add("concert-list__venue");
-    venue.textContent = show.place;
-    concertElement.appendChild(venue);
-    
-    
-    const loacationHead = document.createElement("p");
-    loacationHead.classList.add("concert-list__guide");
-    loacationHead.textContent = "LOCATION";
-    concertElement.appendChild(loacationHead);
-    
-    const location = document.createElement("p");
-    location.classList.add("concert-list__location");
-    location.textContent = show.location;
-    concertElement.appendChild(location);
-    
-    
-    const buyButton = document.createElement("button");
-    buyButton.classList.add("concert-list__buyBtn");
-    buyButton.textContent = "BUY TICKETS";
-    concertElement.appendChild(buyButton);
-    
-    
-    concertList.appendChild(concertElement);
-    
-    });
-    
-
-}
-
-
-
+// displayWords()
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,56 +1,12 @@
 
 
-// // Array of default comments
+// Array of default comments
 
-// // const comments = [
-// //   { url:"../assets/Images/Mohan-muruge.jpg" , name: 'Connor Walton', timestamp:  '02/17/2021', text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' },
-// //   { url:"../assets/Images/Mohan-muruge.jpg" , name: 'Emilie Beach', timestamp:  '01/09/2021', text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.  ' },
-// //   { url:"../assets/Images/Mohan-muruge.jpg" , name: 'Miles Acosta', timestamp:  '12/20/2020', text: 'I cant stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Cant get enough.' },
-// // ];
-
-console.log("herere")
-apiKey = "5218b745-34db-4023-b4da-1a32e81e281d";
-let url = `https://project-1-api.herokuapp.com/comments?api_key=${apiKey}`;
-// function commentsArray (){
-// const arrayOfComments = axios.get("https://project-1-api.herokuapp.com/comments?api_key=5218b745-34db-4023-b4da-1a32e81e281d" )
-
-// .then(response => {
-//   // console.log(response);
-//   displayAllComments(response.data);
-//     // console.log(typeof arrayOfComments);
-//       response.data.sort((a, b) => {
-//         return b.posted - a.posted;
-//       });
-
-
- 
-
-// })
-
-
-
-// .catch(error =>{
-//   console.log(error);
-// });
-
-// }
-const commentsArray = (url) => {
-  axios
-  .get (url)
-  .then(response => {
-
-    console.log(response.data)
-    response.data.sort((a, b) => {
-      return b.timestamp - a.timestamp;
-    });
-    displayAllComments(response.data);
-  })
-  .catch ((err) => {
-
-  })
-}
-commentsArray(url);
-
+const comments = [
+  { url:"../assets/Images/Mohan-muruge.jpg" , name: 'Connor Walton', timestamp:  '02/17/2021', text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' },
+  { url:"../assets/Images/Mohan-muruge.jpg" , name: 'Emilie Beach', timestamp:  '01/09/2021', text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.  ' },
+  { url:"../assets/Images/Mohan-muruge.jpg" , name: 'Miles Acosta', timestamp:  '12/20/2020', text: 'I cant stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Cant get enough.' },
+];
 
 
 // Display a comment on the page
@@ -73,12 +29,12 @@ function displayComment(comment) {
   commentElem.appendChild(nameElem);
 
   const timestampElem = document.createElement('p');
-  timestampElem.textContent =(new Date()).toLocaleDateString();
+  timestampElem.textContent = comment.timestamp;
   timestampElem.classList.add('comment-time');
   commentElem.appendChild(timestampElem);
 
   const textElem = document.createElement('p');
-  textElem.textContent = comment.comment;
+  textElem.textContent = comment.text;
   textElem.classList.add('comment-area');
   commentElem.appendChild(textElem);
 
@@ -86,121 +42,63 @@ function displayComment(comment) {
 }
 
 // Display all comments on the page
-function displayAllComments(arrayOfComments) {
+function displayAllComments() {
   const container = document.getElementById('comment-container');
   container.innerHTML = '';
- 
 
-  arrayOfComments.forEach(comment => {
+  comments.forEach(comment => {
     displayComment(comment);
   });
 }
 
 // Add a new comment
-// function addComment(event) {
-//   event.preventDefault(); // prevent page from reloading
+function addComment(event) {
+  event.preventDefault(); // prevent page from reloading
 
-//   const nameInput = document.getElementById('name-input');
-//   const commentInput = document.getElementById('comment-input');
+  const nameInput = document.getElementById('name-input');
+  const commentInput = document.getElementById('comment-input');
 
-//   const comment = {
-//     url: "../assets/Images/Mohan-muruge.jpg",
-//     name: nameInput.value,
-//     timestamp: (new Date()).toLocaleDateString(),
-//     comment: comment.value,
-//   };
+  const comment = {
+    url: "../assets/Images/Mohan-muruge.jpg",
+    name: nameInput.value,
+    timestamp: Date.now(),
+    text: commentInput.value,
+  };
 
-//   comments.unshift(comment);
-//   nameInput.value = '';
-//   commentInput.value = '';
+  comments.unshift(comment);
+  nameInput.value = '';
+  commentInput.value = '';
 
-//   displayAllComments();
-// }
-
-
-function postComment (name,comment){
-  // const nameInput = document.getElementById('name-input').value;
-  // const commentInput = document.getElementById('comment-input').value;
-  // console.log(nameInput)
-  
-  let newCommentObj = {
-    name: name,
-    comment: comment,
-  }
-  
-  axios
-  .post('https://project-1-api.herokuapp.com/comments?api_key=5218b745-34db-4023-b4da-1a32e81e281d', newCommentObj)
-  .then((response) => {
-    console.log(response);
-  
-   
-    response.sort((a, b) => {
-      return b.timestamp - a.timestamp});
-  
-      
-  })
-
-  .catch((error) => {
-    console.error(error);
-  });
+  displayAllComments();
 }
-
 
 //Event listener to form submit button
 const commentform = document.querySelector('.comment-form');
-commentform.addEventListener('submit', (e) => {
-  e.preventDefault()
-  // e.target.name.value
-  postComment(e.target.name.value, e.target.comment.value);
-});
+commentform.addEventListener('submit', addComment);
 
 // Display default comments on page load
-// displayAllComments();
+displayAllComments();
 
 
 
 
 //Adding all comment section into one div
-// const container = document.getElementById('comment-container');
-// const InfoComment = document.createElement('div');
-// InfoComment.classList.add('comment-Info');
-// container.appendChild(InfoComment);
+const container = document.getElementById('comment-container');
+const InfoComment = document.createElement('div');
+InfoComment.classList.add('comment-Info');
+container.appendChild(InfoComment);
 
 
-// //Appending comments element in created div
-// commentElement = document.querySelector('.comment-element');
-// InfoComment.appendChild(commentElement);
+//Appending comments element in created div
+commentElement = document.querySelector('.comment-element');
+InfoComment.appendChild(commentElement);
 
 
-// commentElement = document.querySelector('.comment-element');
-// InfoComment.appendChild(commentElement);
+commentElement = document.querySelector('.comment-element');
+InfoComment.appendChild(commentElement);
 
-// commentElement = document.querySelector('.comment-element');
-// InfoComment.appendChild(commentElement);
-
-
-
-
-
-//Input field in comment section creating error display
-const nameInput = document.getElementById('name-input');
-const commentInput = document.getElementById('comment-input');
-
-nameInput.addEventListener("blur",validInput);
-commentInput.addEventListener("blur",validInput);
-
-
-
-function validInput(event){
-  const input = event.target;
-  if(input.value === ""){
-    console.log(input);
-
-    input.classList.add("error");
-  }else {
-    input.classList.add("noerror");
-  }
-}
+commentElement = document.querySelector('.comment-element');
+InfoComment.appendChild(commentElement);
 
 
 
@@ -208,9 +106,8 @@ function validInput(event){
 
 
 
-
-
-
-
-
-
+// function timeSet(events){
+//   milsec= timestamp.value;
+//   dt=new Date(milsec * 1000).toLocaleString();
+//   console.log(dt);
+// }
