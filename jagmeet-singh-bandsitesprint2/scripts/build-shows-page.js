b5cd64fa15eb66ad15c34c25dc2dd77c7360ff7b
Jagm33t <130860323+Jagm33t@users.noreply.github.com>
2023-05-19 22:56:00 -0700

    Add files via upload

---
+++ jagmeet-singh-bandsitesprint2/scripts/build-shows-page.js
@@ -0,0 +1,99 @@
+
+//Content stored in Array
+const shows =[
+{date:"Mon Sept 06 2021",venue:"Ronald Lane",location:"San Francisco, CA" } ,
+{date:"Tue Sept 21 2021",venue:"Pier 3 East",location:"San Francisco, CA"},
+{date:"Fri Oct 15 2021",venue:"View Lounge",location:"San Francisco, CA"},
+{date:"Sat Nov 06 2021",venue:"Hyatt Agency",location:"San Francisco, CA"},
+{date:"Fri Nov 26 2021",venue:"Moscow Center",location:"San Francisco, CA"},
+{date:"Wed Dec 15 2021",venue:"Press Club",location:"San Francisco, CA"},
+];
+
+//Selecting classes in comment section
+const concertHead = document.querySelector('.concert-head');
+const concertList = document.querySelector(".concert-list");
+
+
+//Creating heading and appending it.
+const concertHeading = document.createElement('h2');
+concertHeading.classList.add('concert-heading');
+concertHeading.innerText= "Shows";
+concertHead.appendChild(concertHeading);
+
+
+const concert =document.createElement("div");
+concert.classList.add("concert-show");
+concertList.appendChild(concert);
+
+const dateDisplay = document.createElement("span");
+dateDisplay.classList.add("concert-show__display");
+dateDisplay.textContent = "DATE";
+concert.appendChild(dateDisplay);
+
+const venueDisplay =document.createElement("span");
+venueDisplay.classList.add("concert-show__display");
+venueDisplay.textContent = "VENUE";
+concert.appendChild(venueDisplay);
+
+const locationDisplay = document.createElement("span");
+locationDisplay.classList.add("concert-show__display");
+locationDisplay.textContent = "LOCATION";
+concert.appendChild(locationDisplay);
+
+const emptyDisplay = document.createElement("span");
+emptyDisplay.classList.add("concert-show__empty");
+emptyDisplay.textContent = " ";
+concert.appendChild(emptyDisplay);
+
+
+
+//Creating elements and looping through it.
+shows.forEach(show =>{
+
+const concertElement= document.createElement("div");
+concertElement.classList.add("concert-list__element");
+
+const dateHead = document.createElement("p");
+dateHead.classList.add("concert-list__guide");
+dateHead.textContent = "DATE";
+concertElement.appendChild(dateHead);
+
+const date = document.createElement("p");
+date.classList.add("concert-list__date");
+date.textContent = show.date;
+concertElement.appendChild(date);
+
+
+const venueHead = document.createElement("p");
+venueHead.classList.add("concert-list__guide");
+venueHead.textContent = "VENUE";
+concertElement.appendChild(venueHead);
+
+const venue = document.createElement("p");
+venue.classList.add("concert-list__venue");
+venue.textContent = show.venue;
+concertElement.appendChild(venue);
+
+
+const loacationHead = document.createElement("p");
+loacationHead.classList.add("concert-list__guide");
+loacationHead.textContent = "LOCATION";
+concertElement.appendChild(loacationHead);
+
+const location = document.createElement("p");
+location.classList.add("concert-list__location");
+location.textContent = show.location;
+concertElement.appendChild(location);
+
+
+const buyButton = document.createElement("button");
+buyButton.classList.add("concert-list__buyBtn");
+buyButton.textContent = "BUY TICKETS";
+concertElement.appendChild(buyButton);
+
+
+concertList.appendChild(concertElement);
+
+});
+
+
---
+++ jagmeet-singh-bandsitesprint2/scripts/index-page.js
@@ -0,0 +1,126 @@
+
+
+// Array of default comments
+
+const comments = [
+  { url:"../assets/Images/Mohan-muruge.jpg" , name: 'Connor Walton', timestamp:  '02/17/2021', text: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.' },
+  { url:"../assets/Images/Mohan-muruge.jpg" , name: 'Emilie Beach', timestamp:  '01/09/2021', text: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.  ' },
+  { url:"../assets/Images/Mohan-muruge.jpg" , name: 'Miles Acosta', timestamp:  '12/20/2020', text: 'I cant stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Cant get enough.' },
+];
+
+
+// Display a comment on the page
+function displayComment(comment) {
+  const container = document.getElementById('comment-container');
+
+
+  const commentElem = document.createElement('div');
+  commentElem.classList.add('comment-element');
+
+
+  const imgElem = document.createElement('img');
+  imgElem.classList.add('comment-image');
+  imgElem.setAttribute('src', comment.url);
+  commentElem.appendChild(imgElem);
+
+  const nameElem = document.createElement('h3');
+  nameElem.textContent = comment.name;
+  nameElem.classList.add('comment-name');
+  commentElem.appendChild(nameElem);
+
+  const timestampElem = document.createElement('p');
+  timestampElem.textContent = comment.timestamp;
+  timestampElem.classList.add('comment-time');
+  commentElem.appendChild(timestampElem);
+
+  const textElem = document.createElement('p');
+  textElem.textContent = comment.text;
+  textElem.classList.add('comment-area');
+  commentElem.appendChild(textElem);
+
+  container.appendChild(commentElem);
+}
+
+// Display all comments on the page
+function displayAllComments() {
+  const container = document.getElementById('comment-container');
+  container.innerHTML = '';
+
+  comments.forEach(comment => {
+    displayComment(comment);
+  });
+}
+
+// Add a new comment
+function addComment(event) {
+  event.preventDefault(); // prevent page from reloading
+
+  const nameInput = document.getElementById('name-input');
+  const commentInput = document.getElementById('comment-input');
+
+  const comment = {
+    url: "../assets/Images/Mohan-muruge.jpg",
+    name: nameInput.value,
+    timestamp: (new Date()).toLocaleDateString(),
+    text: commentInput.value,
+  };
+
+  comments.unshift(comment);
+  nameInput.value = '';
+  commentInput.value = '';
+
+  displayAllComments();
+}
+
+//Event listener to form submit button
+const commentform = document.querySelector('.comment-form');
+commentform.addEventListener('submit', addComment);
+
+// Display default comments on page load
+displayAllComments();
+
+
+
+
+//Adding all comment section into one div
+const container = document.getElementById('comment-container');
+const InfoComment = document.createElement('div');
+InfoComment.classList.add('comment-Info');
+container.appendChild(InfoComment);
+
+
+//Appending comments element in created div
+commentElement = document.querySelector('.comment-element');
+InfoComment.appendChild(commentElement);
+
+
+commentElement = document.querySelector('.comment-element');
+InfoComment.appendChild(commentElement);
+
+commentElement = document.querySelector('.comment-element');
+InfoComment.appendChild(commentElement);
+
+
+
+
+
+//Input field in comment section creating error display
+const nameInput = document.getElementById('name-input');
+const commentInput = document.getElementById('comment-input');
+
+nameInput.addEventListener("blur",validInput);
+commentInput.addEventListener("blur",validInput);
+
+
+
+function validInput(event){
+  const input = event.target;
+  if(input.value === ""){
+    console.log(input);
+
+    input.classList.add("error");
+  }else {
+    input.classList.add("noerror");
+  }
+}
+  
